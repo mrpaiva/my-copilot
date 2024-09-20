0bb072558088822b4b056527df4fbf44a618fade
Mauro Couto de Paiva <mauro.paiva@triadesolucoes.com.br>
2024-09-20 09:34:59 -0300

    atualizando saudação do copilot

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.title("MultiClubes Copilot")
 # session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
-        AIMessage(content="Oi, eu sou seu copiloto. Estou sendo treinado para responder suas principais dúvidas sobre os produtos MultiClubes. Como posso te ajudar?"),
+        AIMessage(content="Olá, eu sou seu copiloto! Ainda sou um protótipo, então tenha paciência comigo 😊 Estou aqui para te ajudar com suas dúvidas sobre os produtos MultiClubes. O que posso fazer por você hoje?"),
     ]
 
     
